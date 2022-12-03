1f3a7733ed98938888c03256fa57b9b962f89b31
Alex <alexsa-8@mail.ru>
2022-12-03 13:14:37 +0300

    книга сотрудников

--- src/main/java/com/skypro/employee_book/controller/EmployeeController.java
+++ src/main/java/com/skypro/employee_book/controller/EmployeeController.java
@@ -6,10 +6,11 @@ import com.skypro.employee_book.service.EmployeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
 import java.util.List;
-
+@RestController
 public class EmployeeController {
     private final EmployeeService employeeService;
 
---
+++ src/main/java/com/skypro/employee_book/service/DepartmentService.java
@@ -0,0 +1,55 @@
+package com.skypro.employee_book.service;
+
+import com.skypro.employee_book.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+public class DepartmentService {
+    private final EmployeeService employeeService;
+
+    public DepartmentService(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    public int getSumSalaryDepartment(int deportment){
+        return getEmployeeDepartment(deportment)
+                .mapToInt(Employee::getSalary)
+                .sum();
+    }
+
+    public int getMaxSalaryDepartment(int department){
+        return getEmployeeDepartment(department)
+                .mapToInt(Employee::getSalary)
+                .max()
+                .orElseThrow(RuntimeException::new);
+    }
+
+    public int getMinSalaryDepartment(int department){
+        return getEmployeeDepartment(department)
+                .mapToInt(Employee::getSalary)
+                .min()
+                .orElseThrow(RuntimeException::new);
+    }
+    public List<Employee> getEmployeeByDepartment(int deportment){
+        return employeeService
+                .getAllEmployees()
+                .stream()
+                .filter(employee -> employee.getDepartment()==deportment)
+                .collect(Collectors.toList());
+    }
+    public Map<Integer,List<Employee>> getEmployeesGroupedByDepartment(){
+        return employeeService.getAllEmployees()
+                .stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
+    }
+    public Stream<Employee> getEmployeeDepartment(int department){
+        return employeeService.getAllEmployees()
+                .stream()
+                .filter(employee -> employee.getDepartment()==department);
+    }
+}
